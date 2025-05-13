795793b3889e40f2964c194cabdb7e7fc608175b
Farbod Rahiminik <Farbod@Negars-MacBook-Pro.local>
2025-05-13 15:58:41 +0330

    Added Flippable view

---
+++ AppUI/Sources/AppUI/Foundation/FlippableView.swift
@@ -0,0 +1,59 @@
+//
+//  FlippableView.swift
+//  AppUI
+//
+//  Created by Farbod Rahiminik on 5/13/25.
+//
+
+import SwiftUI
+
+struct FlippableView<Front: View, Back: View>: View {
+    @State private var isFlipped = false
+    
+    let front: Front
+    let back: Back
+    let animationDuration: Double
+    let perspective: CGFloat
+    
+    init(
+        animationDuration: Double = 0.6,
+        perspective: CGFloat = 0.8,
+        @ViewBuilder front: () -> Front,
+        @ViewBuilder back: () -> Back
+    ) {
+        self.front = front()
+        self.back = back()
+        self.animationDuration = animationDuration
+        self.perspective = perspective
+    }
+    
+    var body: some View {
+        ZStack {
+            front
+                .opacity(isFlipped ? 0 : 1)
+                .rotation3DEffect(
+                    .degrees(isFlipped ? 180 :   0),
+                    axis: (x: 0, y: 1, z: 0),
+                    perspective: perspective
+                )
+            
+            back
+                .opacity(isFlipped ? 1 : 0)
+                .rotation3DEffect(
+                    .degrees(isFlipped ?   0 : -180),
+                    axis: (x: 0, y: 1, z: 0),
+                    perspective: perspective
+                )
+        }
+        .animation(.easeInOut(duration: animationDuration), value: isFlipped)
+        .onContinuousHover(coordinateSpace: .local) { phase in
+            switch phase {
+            case .ended:
+                isFlipped.toggle()
+                
+            default:
+                break
+            }
+        }
+    }
+}
